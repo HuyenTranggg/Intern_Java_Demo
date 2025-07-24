d3b666783797603eeb07514b32e9fd2d7c36e98e
HaiYen3012 <lehaiyen30122004@gmail.com>
2025-07-24 12:26:44 +0700

    update

--- SpringCore/DataAccess_SpringORM/dao/UserDao.java
+++
@@ -1,6 +0,0 @@
-package SpringCore.DataAccess_SpringORM.dao;
-
-public interface UserDao {
-    void save(User user);
-    List<User> getAll();
-}
--- SpringCore/DataAccess_SpringORM/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package SpringCore.DataAccess_SpringORM.service;
-
-public interface UserService {
-    void registerUser(String name);
-    void listUsers();
-}
