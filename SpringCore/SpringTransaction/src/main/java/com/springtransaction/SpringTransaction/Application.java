40480566d79bb5f1c7a8814525e0d37b5505bd93
btttrangcfm09 <soratrang31294@gmail.com>
2025-07-24 15:57:34 +0700

    thuoc tinh propagation cua @transactional

--- SpringCore/SpringTransaction/src/main/java/com/springtransaction/SpringTransaction/Application.java
+++ SpringCore/SpringTransaction/src/main/java/com/springtransaction/SpringTransaction/Application.java
@@ -5,6 +5,8 @@ import com.springtransaction.SpringTransaction.config.AppConfig;
 import com.springtransaction.SpringTransaction.dao.ProductDao;
 import com.springtransaction.SpringTransaction.entity.Product;
 import com.springtransaction.SpringTransaction.service.DeclarativeProductService;
+import com.springtransaction.SpringTransaction.service.InnerService;
+import com.springtransaction.SpringTransaction.service.OuterService;
 import com.springtransaction.SpringTransaction.service.ProgrammaticProductService;
 
 public class Application{
@@ -13,35 +15,46 @@ public class Application{
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		
 		// lay bean
-		DeclarativeProductService declarativeService = context.getBean(DeclarativeProductService.class);
-		ProgrammaticProductService programmaticService = context.getBean(ProgrammaticProductService.class);
+//		DeclarativeProductService declarativeService = context.getBean(DeclarativeProductService.class);
+//		ProgrammaticProductService programmaticService = context.getBean(ProgrammaticProductService.class);
 		ProductDao productDao = context.getBean(ProductDao.class);
+//		
+//		// khoi tao
+//		System.out.println(productDao.count());
+//		
+//		// Declarative - Fail case
+//		try {
+//			declarativeService.createProductAndFail(new Product("Fail"));
+//		}catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		System.out.println(productDao.count());
+//		
+//		//Declarative - success case
+//		declarativeService.createProductSuccess(new Product("Laptop"));
+//		System.out.println(productDao.count());
+//		
+//		// Programmatic - fail case
+//		try {
+//			programmaticService.createProductAndFail(new Product("fail"));
+//		}catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		System.out.println(productDao.count());
+//		
+//		// Programmatic -success case
+//		programmaticService.createProductSusscess(new Product("Laptop"));
+//		System.out.println(productDao.count());
 		
-		// khoi tao
-		System.out.println(productDao.count());
 		
-		// Declarative - Fail case
+		// Demo Propagation
+		OuterService outerService = context.getBean(OuterService.class);
+		InnerService innerService = context.getBean(InnerService.class);
+		
+		// REQUIRED
 		try {
-			declarativeService.createProductAndFail(new Product("Fail"));
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-		System.out.println(productDao.count());
-		
-		//Declarative - success case
-		declarativeService.createProductSuccess(new Product("Laptop"));
-		System.out.println(productDao.count());
-		
-		// Programmatic - fail case
-		try {
-			programmaticService.createProductAndFail(new Product("fail"));
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-		System.out.println(productDao.count());
-		
-		// Programmatic -success case
-		programmaticService.createProductSusscess(new Product("Laptop"));
+			outerService.testRequired_InnerFails();
+		} catch(Exception e) {}
 		System.out.println(productDao.count());
 		
 		// Giai phong du lieu va dong Spring container
